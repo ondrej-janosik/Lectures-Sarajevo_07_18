5b5d527e5c3f697a126d08f9659f1c14d0c0b5bb
Ondřej Janošík <ondrej.janosik42@gmail.com>
2018-07-15 21:53:31 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app" class="container">
+    <img width="25%" src="./assets/logo.png">
+    <HelloWorld/>
+    {{ text }}
+    <input v-model="text"/>
+    <Counter :startCount="6" @increased="show"/>
+    <Counter :startCount="0"/>
+    <Counter :startCount="12"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import Counter from "./components/Counter";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      text: "Hi, how are you?",
+      something: 5
+    };
+  },
+  methods: {
+    show(count) {
+      alert(count);
+    }
+  },
+  components: {
+    HelloWorld,
+    Counter
+  }
+};
+</script>
+
+<style>
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+  {{ count }}
+    <button @click="increase">Click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["startCount"],
+  data() {
+    return {
+      count: this.startCount
+    };
+  },
+  methods: {
+    increase() {
+      this.count += 1;
+      this.$emit('increased', this.count)
+    }
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  background-color: yellow;
+}
+</style>
